94c51761f8ae744051a372c64087109f41aa23dd
Gary Slopsema <gslopsema@llnw.com>
2014-05-10 10:36:18 -0400

    Added some basic logging

--- steamail/code_extractor.go
+++ steamail/code_extractor.go
@@ -6,6 +6,7 @@ package steamail
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/mail"
 	"regexp"
 )
@@ -17,22 +18,30 @@ func NewSteamCodeExtractor() *steamCodeExtractor {
 }
 
 func (e *steamCodeExtractor) ExtractCode(msg *mail.Message) (string, error) {
+	log.Printf("Attempting to extract code from email")
 	body, err := e.extractBody(msg)
 	if err != nil {
+		log.Printf("Couldn't read message body: %v", err)
 		return "", fmt.Errorf("could not extract message body: %v", err)
 	}
-	return e.extractCode(body), nil
+	return e.extractCode(body)
 }
 
 func (e *steamCodeExtractor) extractBody(msg *mail.Message) ([]byte, error) {
 	return ioutil.ReadAll(msg.Body)
 }
 
-func (e *steamCodeExtractor) extractCode(body []byte) string {
+func (e *steamCodeExtractor) extractCode(body []byte) (string, error) {
 	// The email has only one <h3>...</h3> block; the code is wrapped in
 	// that.
 	codeRegexp := regexp.MustCompile("<h3>.*</h3>")
+	if !codeRegexp.Match(body) {
+		log.Printf("No regexp match for code in email")
+		return "", fmt.Errorf("no code in email")
+	}
 	codeBytes := codeRegexp.Find(body)
 	// Eh, ugly, but whatever
-	return string(codeBytes[4 : len(codeBytes)-5])
+	code := string(codeBytes[4 : len(codeBytes)-5])
+	log.Printf("Extracted auth code '%s'", code)
+	return code, nil
 }
